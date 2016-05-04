a959fda741f7ffe826dff4b0b9e139a1f1c2e7c1
yhl221 <1727538058@qq.com>
2016-05-04 22:25:02 +0800

    finish main.js

--- lcdDigits_v1/main/main.js
+++ lcdDigits_v1/main/main.js
@@ -1,3 +1,39 @@
-/**
- * Created by yhl on 16-5-4.
- */
+function printGrid(digit) {
+
+    var allGrid = loadAllDigits();
+    var digitArray = buildDigitArray(digit);
+    var digitGrid = buildDigitGrid(digitArray, allGrid);
+    var finalPrint = buildFinalPrint(digitGrid);
+    console.log(finalPrint);
+}
+
+function buildDigitArray(digit) {
+    var digitArray = [];
+
+    var digitString = digit.toString();
+    for (var i = 0; i < digitString.length; i++)
+        digitArray.push(digitString.charAt(i));
+
+    return digitArray;
+}
+
+function buildDigitGrid(digitArray, allGrid) {
+    var digitGrid = [];
+
+    digitArray.forEach(function (key) {
+        digitGrid.push(allGrid[key]);
+    });
+
+    return digitGrid;
+}
+
+function buildFinalPrint(digitGrid) {
+    var finalPrint = '';
+    
+    digitGrid.forEach(function (key) {
+        var r = Object.keys(key);
+        finalPrint = finalPrint + key[r];
+    });
+
+    return finalPrint;
+}
